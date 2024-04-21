58054e8c62713f2cd32e37e9ffbbefa86d71ce81
wytzig <luukweiss@gmail.com>
2024-04-21 22:03:58 +0200

    ugh

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,5 +5,5 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-    //todo do a class?
+    //todo do a record?
 }
